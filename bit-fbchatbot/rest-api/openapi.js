c20962ea6717c63a137a92703386a1808f5dbc06
EC2 Default User <ec2-user@ip-172-31-9-182.ap-northeast-2.compute.internal>
2017-11-22 10:20:10 +0000

    ,

--- bit-fbchatbot/rest-api/openapi.js
+++ bit-fbchatbot/rest-api/openapi.js
@@ -3,14 +3,14 @@ const request = require('request')
 const searchNewAddress = (type, searchWord) => {
 
     var url = 'http://openapi.epost.go.kr/postal/retrieveNewAdressAreaCdService/retrieveNewAdressAreaCdService/getNewAddressListAreaCd';
-    var queryParams = '?' + 'ServiceKey' + '=7zzoOgBYZmF97yjzJb7C3cgqXqCe1ImhV21M93fB8BTiVfhBhOgD4I9Mr3Hd3NE8AnpalmxuYoNwLOAUkafA1Q%3D%3D'; /* Service Key*/
-    queryParams += '&searchSe=' + type; /* dong : 동(읍/면)명 road :도로명[default] post : 우편번호 */
-    queryParams += '&srchwrd=' + encodeURIComponent('searchWord'); /* 검색어 */
-    queryParams += '&countPerPage=10'; /* 페이지당 출력될 개수를 지정 */
-    queryParams += '&currentPage=1'; /* 출력될 페이지 번호 */
+    var queryString = '?ServiceKey=' + '7zzoOgBYZmF97yjzJb7C3cgqXqCe1ImhV21M93fB8BTiVfhBhOgD4I9Mr3Hd3NE8AnpalmxuYoNwLOAUkafA1Q%3D%3D'; /* Service Key*/
+    queryString += '&searchSe=' + type; /* dong : 동(읍/면)명 road :도로명[default] post : 우편번호 */
+    queryString += '&srchwrd=' + encodeURIComponent(searchWord);
+    queryString += '&countPerPage=10'; /* 페이지당 출력될 개수를 지정 */
+    queryString += '&currentPage=1'; /* 출력될 페이지 번호 */
 
     request({
-        uri: url + queryParams,
+        uri: url + queryString,
         // uri: 'http://openapi.epost.go.kr/postal/retrieveNewAdressAreaCdService/retrieveNewAdressAreaCdService/getNewAddressListAreaCd',
         // qs: {
         //     'ServiceKey': '7zzoOgBYZmF97yjzJb7C3cgqXqCe1ImhV21M93fB8BTiVfhBhOgD4I9Mr3Hd3NE8AnpalmxuYoNwLOAUkafA1Q%3D%3D',
@@ -25,7 +25,9 @@ const searchNewAddress = (type, searchWord) => {
         console.log('=>Reponse received', body);
     });
 };
-searchNewAddress('road', '양도로18')
+
+searchNewAddress('road','양도로 18');
+
 module.exports = {
     // searchNewAddress
 };
